2dd7ac7d863fd4ab82a9db61a9c956486b895ffa
IvanTabernakulov <ivan.tabernakulov@gmail.com>
2015-12-10 15:21:58 +0200

    Update content.js

--- content.js
+++ content.js
@@ -19,6 +19,7 @@ for (var i = 0; i < elements.length; i++) {
             replacedText = replacedText.replace(/\bnews/gi, 'memes');
             replacedText = replacedText.replace(/\bfaso/gi, 'fatboi');
             replacedText = replacedText.replace(/\bcamelta/gi, 'gotta');
+            replacedText = replacedText.replace(/\b tight, /gi, ' tight, unlike ur mom's vag, ');
             replacedText = replacedText.replace(/\b tight /gi, ' tight, unlike ur mom's vag, ');
             replacedText = replacedText.replace(/\b tight./gi, ' tight, unlike ur mom's vag.');
 
